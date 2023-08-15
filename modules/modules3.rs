033dd2a36736e7a9882affe9c2861f15d36d3d67
Javier Carrasco <javier.carrasco.cruz@gmail.com>
2023-08-15 19:00:03 +0200

    modules: solutions

--- modules/modules3.rs
+++ modules/modules3.rs
@@ -5,10 +5,9 @@
 // from the std::time module. Bonus style points if you can do it with one line!
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-// TODO: Complete this use statement
-use ???
+use std::time::SystemTime as SystemTime;
+use std::time::UNIX_EPOCH as UNIX_EPOCH;
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
