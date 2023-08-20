babfd92a35955d8180a250f8a38734cd8099b569
Javier Carrasco <javier.carrasco.cruz@gmail.com>
2023-08-20 14:45:55 +0200

    iterators: solutions

--- iterators/iterators4.rs
+++ iterators/iterators4.rs
@@ -1,7 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +12,12 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    //range [1..num] and then use fold to itreate
+    // (1..=num).fold(1, |acc, v| acc * v)
+    //a more elegant solution with the product trait,
+    //which already implements the closure:
+    (1..=num).product()
 }
 
 #[cfg(test)]
--- iterators/iterators5.rs
+++ iterators/iterators5.rs
@@ -8,7 +8,6 @@
 // need to be modified.
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -32,7 +31,9 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    // values() -> iterator over the values of HashMap. The returned value is &V.
+    // filter() -> dereference and compare to count()
+    map.values().filter(|&x| *x == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -51,7 +52,8 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    // use count_iterator() in a map() and sum the results
+    collection.iter().map(|m| count_iterator(m, value)).sum()
 }
 
 #[cfg(test)]
